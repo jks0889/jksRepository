f75e56021e1dfc2c5541219ad434ff5262b86c7c
jks-pc <jks0889@naver.com>
2016-05-23 20:01:53 +0900

    third

--- app/src/main/java/kr/jks/test/MainActivity.java
+++ app/src/main/java/kr/jks/test/MainActivity.java
@@ -1,14 +1,26 @@
 package kr.jks.test;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.melnykov.fab.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ListView lv = (ListView) findViewById(android.R.id.list);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        //Sfab.attachToListView(lv);
+
+
     }
+
 }
