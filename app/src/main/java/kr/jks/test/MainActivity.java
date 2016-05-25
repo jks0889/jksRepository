12c813c21de5ba0db98de06b0e156d52af44f736
jks-pc <jks0889@naver.com>
2016-05-25 22:08:29 +0900

    custom list

--- app/src/main/java/kr/jks/test/MainActivity.java
+++ app/src/main/java/kr/jks/test/MainActivity.java
@@ -3,9 +3,12 @@ package kr.jks.test;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.melnykov.fab.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,9 +20,20 @@ public class MainActivity extends AppCompatActivity {
 
         ListView lv = (ListView) findViewById(android.R.id.list);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.attachToListView(lv);
 
-        //Sfab.attachToListView(lv);
-        //heell jks 2016 05 23
+        ArrayList<MyItem> arItems  = new ArrayList<>();
+        MyItem item;
+        item = new MyItem(R.drawable.fab_bg_mini,"1번");
+        arItems.add(item);
+        item = new MyItem(R.drawable.fab_shadow,"1번");
+        arItems.add(item);
+        item = new MyItem(R.drawable.fab_bg_normal,"1번");
+        arItems.add(item);
+
+        MyListAdapter mAdapter = new MyListAdapter(this,arItems,R.layout.icontext);
+        lv.setAdapter(mAdapter);
+
 
     }
 
